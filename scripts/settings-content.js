e5b709c8f7c6b27b651e3534cae9524f609ace5d
SAMPPLUS <sampeterson560@gmail.com>
2023-06-29 10:26:42 -0600

    submittable quill RTE on user settings page

--- scripts/settings-content.js
+++ scripts/settings-content.js
@@ -2,7 +2,29 @@ const SETTINGS_CLASSLIST = ["body-text", "-small"];
 
 waitForElm('.app-available-settings').then((container)=>{
     waitForElm('textarea', container).then((text_area) => {
-        insertFormatRow(text_area, SETTINGS_CLASSLIST);
+        text_area.insertAdjacentHTML('beforebegin', '<div id="editor"><p>Hello World!</p><p>Some initial <strong>bold</strong> text</p><p><br></p></div>');
+        var hinput = document.createElement('input');
+        hinput.type='hidden';
+        hinput.name='bio';
+        text_area.insertAdjacentElement('afterend', hinput);
+
+        var quill = new Quill('#editor', {
+            theme: 'snow',
+            formats: ['bold', 'italic', 'blockquote', 'link'],
+            modules: {
+                toolbar: ['bold', 'italic', 'blockquote', 'link']
+            }
+        });
+        quill.on('text-change', () => {
+            let str = quill.root.innerHTML.replaceAll('<p>','').replaceAll('</p>', '\n').replaceAll('<br>','');
+            console.log(str);
+            hinput.value = str;
+        });
+        text_area.insertAdjacentHTML('afterbegin', '<input name="bio" type="hidden"></input>')
+        text_area.style.display = 'none';
+        text_area.name = 'ta';
+        
+        //insertFormatRow(text_area, SETTINGS_CLASSLIST);
     });
 });
 
